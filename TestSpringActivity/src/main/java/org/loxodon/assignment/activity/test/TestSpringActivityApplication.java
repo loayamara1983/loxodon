41027fa413981dd6fd36ba0e8db51bd387a1b524
Loay <Loay@DESKTOP-7852KET>
2018-05-12 13:31:02 -0700

    test commit

---
+++ TestSpringActivity/src/main/java/org/loxodon/assignment/activity/test/TestSpringActivityApplication.java
@@ -0,0 +1,12 @@
+package org.loxodon.assignment.activity.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringActivityApplication.class, args);
+	}
+}
---
+++ TestSpringActivity/src/test/java/org/loxodon/assignment/activity/test/TestSpringActivityApplicationTests.java
@@ -0,0 +1,16 @@
+package org.loxodon.assignment.activity.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestSpringActivityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
